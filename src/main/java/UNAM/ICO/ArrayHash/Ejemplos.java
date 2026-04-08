a427ac6412d05cef723a468bd0a6dbec3ebba280
Fercho182820 <Fercho182820@icloud.com>
2026-04-08 14:39:42 -0600

    For each

--- src/main/java/UNAM/ICO/ArrayHash/Ejemplos.java
+++ src/main/java/UNAM/ICO/ArrayHash/Ejemplos.java
@@ -21,6 +21,25 @@ public class Ejemplos {
 
         System.out.println("El estudiante " + listaEstudiantes.get(1) + " tiene una calificacion de " + calificaciones.get("Carrillo"));
 
+        //Garcia entrego un proyecto
+        calificaciones.put("Garcia",10);
+        if (calificaciones.containsKey("Perez")) {
+            System.out.println("Calificacion: " + calificaciones.get("Perez"));
+        } else {
+            System.out.println("El alumno no se encuentra en el sistema");
+        }
+        //Villalba se cambio de escuela
+        listaEstudiantes.remove(2);
+        calificaciones.remove("Villalba");
+        System.out.println(listaEstudiantes.get(0) + calificaciones.get("Garcia"));
+
+        //for each
+        for (String nombre : listaEstudiantes){
+            System.out.println("Alumno: " + nombre + " | Calificacion: " +  calificaciones.get(nombre));
+        }
+
+
+
 
     }
 
